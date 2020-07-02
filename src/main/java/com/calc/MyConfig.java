a9447108cc83b329cbe05ac9b6bce4655b5144ef
Piyush Subhash Atram <piyusha947@gmail.com>
2020-07-02 18:44:30 +0530

    addition operation without xml

---
+++ src/main/java/com/calc/MyConfig.java
@@ -0,0 +1,13 @@
+package com.calc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.calc")
+public class MyConfig extends WebMvcConfigurerAdapter {
+
+}
---
+++ src/main/java/com/calc/MyFrontController.java
@@ -0,0 +1,25 @@
+package com.calc;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyFrontController extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {MyConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[] {"/"};
+	}
+
+}
